1bb9e125ad274e5bfb42eeac2fb089c52f3231d4
tominehaug <tomine_haug@live.no>
2026-04-16 14:24:33 +0200

    logic to display posts in feed with renderPosts

--- js/pages/feed.js
+++ js/pages/feed.js
@@ -19,4 +19,43 @@ async function fetchPosts(endpoint) {
   }
 }
 
-async function renderPosts() {}
+async function renderPosts(posts) {
+  const container = document.getElementById("posts-feed");
+  container.innerHTML = "";
+  posts.forEach((post) => {
+    const postWrapper = document.createElement("div");
+    postWrapper.classList.add("post");
+
+    postWrapper.addEventListener("click", () => {
+      window.location.href = `post.html?id=${post.id}`;
+    });
+
+    const linkUser = document.createElement("a");
+    linkUser.href = `profile.html?user=${post.author}`;
+    linkUser.addEventListener("click", (e) => {
+      e.stopPropagation();
+    });
+
+    const username = document.createElement("h3");
+    username.textContent = post.author;
+
+    linkUser.appendChild(username);
+
+    const media = document.createElement("img");
+    media.src = post.media?.url || "";
+    media.alt = post.media?.alt || "";
+
+    const title = document.createElement("h2");
+    title.textContent = post.title;
+
+    const caption = document.createElement("p");
+    caption.textContent = post.body || "";
+
+    postWrapper.appendChild(linkUser);
+    postWrapper.appendChild(media);
+    postWrapper.appendChild(title);
+    postWrapper.appendChild(caption);
+
+    container.appendChild(postWrapper);
+  });
+}
