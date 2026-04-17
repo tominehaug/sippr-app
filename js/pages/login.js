c039488946cc955e3a86cf93f1ea30f870c5d882
tominehaug <tomine_haug@live.no>
2026-04-17 11:30:55 +0200

    fixed import paths

---
+++ js/pages/login.js
@@ -0,0 +1,30 @@
+import { validateForm } from "../utils/validation.js";
+import { loginUser } from "../services/authService.js";
+import { showError } from "../services/errors.js";
+
+const loginForm = document.getElementById("login-form");
+
+loginForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const isValid = validateForm(loginForm);
+
+  if (!isValid) return;
+
+  await handleLogin();
+});
+
+async function handleLogin() {
+  // collect login data
+  const formData = new FormData(loginForm);
+  const body = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
+  // save credentials
+  try {
+    await loginUser(body);
+    window.location.href = "../../feed.html";
+  } catch (error) {
+    showError(error.message || "Login failed");
+  }
+}
