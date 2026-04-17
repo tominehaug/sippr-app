0626d89fb12bbc70a2a66cc69a5283525a925f46
tominehaug <tomine_haug@live.no>
2026-04-17 11:56:14 +0200

    added logic to display single post

---
+++ js/pages/post.js
@@ -0,0 +1,9 @@
+import { createPostCard } from "../components/postCard.js";
+
+async function renderSinglePost(post) {
+  const container = document.getElementById("post-container");
+  container.innerHTML = "";
+
+  const postCard = createPostCard(post);
+  container.appendChild(postCard);
+}
