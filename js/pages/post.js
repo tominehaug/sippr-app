c33a216f72e83e253fa1e51d9ec6001973e15881
tominehaug <tomine_haug@live.no>
2026-04-20 11:54:46 +0200

    added fetch logic for single post

--- js/pages/post.js
+++ js/pages/post.js
@@ -1,4 +1,19 @@
 import { createPostCard } from "../components/postCard.js";
+import { get } from "../services/apiClient.js";
+
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+async function fetchSinglePost() {
+  try {
+    const data = await get(`/social/posts/${id}?_author=true`);
+    const post = data.data;
+
+    renderSinglePost(post);
+  } catch (error) {
+    showError(error.message || "Something went wrong when fetching the post.");
+  }
+}
 
 async function renderSinglePost(post) {
   const container = document.getElementById("post-container");
@@ -8,4 +23,4 @@ async function renderSinglePost(post) {
   container.appendChild(postCard);
 }
 
-document.addEventListener("DOMContentLoaded", renderSinglePost);
+document.addEventListener("DOMContentLoaded", fetchSinglePost);
