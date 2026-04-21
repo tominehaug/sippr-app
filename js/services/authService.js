ea3744915a242fc530630804e43aa48b022ccd97
tominehaug <tomine_haug@live.no>
2026-04-21 14:11:40 +0200

    removed error handling. only handled in login.js.

--- js/services/authService.js
+++ js/services/authService.js
@@ -1,23 +1,15 @@
 import { post } from "./apiClient.js";
-import { showError } from "./errors.js";
 
 const LOGIN_ENDPOINT = "/auth/login";
 
 export async function loginUser(credentials) {
-  try {
-    const response = await post(LOGIN_ENDPOINT, credentials);
-    const { accessToken, ...profile } = response.data;
+  const response = await post(LOGIN_ENDPOINT, credentials);
+  const { accessToken, ...profile } = response.data;
 
-    if (accessToken) {
-      localStorage.setItem("accessToken", accessToken);
-      localStorage.setItem("profile", JSON.stringify(profile));
-      return profile;
-    } else {
-      throw new Error("Login successful, but no access token received.");
-    }
-  } catch (error) {
-    showError(error.message || "Login failed");
-    throw error;
+  if (accessToken) {
+    localStorage.setItem("accessToken", accessToken);
+    localStorage.setItem("profile", JSON.stringify(profile));
+    return profile;
   }
 }
 
