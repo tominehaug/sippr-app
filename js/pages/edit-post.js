985400ed7d53cc2cfc110c260ef535bfc1322358
tominehaug <tomine_haug@live.no>
2026-04-21 16:47:58 +0200

    wrote logic for editing a post

---
+++ js/pages/edit-post.js
@@ -0,0 +1,64 @@
+import { post } from "../services/apiClient.js";
+import { showError } from "../services/errors.js";
+import { showMessage } from "../services/ui-messages.js";
+import { validateForm } from "../utils/validation.js";
+
+const editForm = document.getElementById("create-form");
+const params = new URLSearchParams(window.location.search);
+const postId = params.get("id");
+
+async function fetchPost() {
+  try {
+    const data = await get(`/social/posts/${postId}`);
+    const post = data.data;
+    renderEditForm(post);
+  } catch (error) {
+    showError(error.message || "Something went wrong when fetching the post.");
+  }
+}
+
+async function renderEditForm(post) {
+  document.getElementById("img-url").value = post.media.url || "";
+  document.getElementById("img-url").value = post.media.alt || "";
+  document.getElementById("title").value = post.title || "";
+  document.getElementById("caption").value = post.body || "";
+}
+
+editForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const isValid = validateForm(editForm);
+
+  if (!isValid) return;
+
+  await updatePost(editForm);
+});
+
+async function updatePost(form) {
+  const formData = new FormData(form);
+
+  const body = {
+    title: formData.get("title"),
+    body: formData.get("caption"),
+    media: {
+      url: formData.get("img-url"),
+      alt: formData.get("img-alt"),
+    },
+  };
+
+  const submitBtn = createForm.getElementById("submit-btn");
+  submitBtn.disabled = true;
+
+  try {
+    await put(`/social/posts/${postId}`, body);
+    showMessage("Upload was successful!");
+    setTimeout(() => {
+      window.location.href = "../../feed.html";
+    }, 1000);
+  } catch (error) {
+    showError(error?.message || "Upload failed");
+  } finally {
+    submitBtn.disabled = false;
+  }
+}
+
+// delete post
