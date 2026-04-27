4a56b975bd76edd6ab542457cc41ee80a5f75825
tominehaug <tomine_haug@live.no>
2026-04-27 18:37:02 +0200

    adjusted size on logo

--- js/components/header.js
+++ js/components/header.js
@@ -16,13 +16,13 @@ export function renderHeader() {
   if (path.includes("feed.html")) {
     header.innerHTML = `
     <a href="feed.html">
-        <img src="assets/logo.svg" alt="Sippr logo" width="86" height="35"/>
+        <img src="assets/logo.svg" alt="Sippr logo" width="148" height="58"/>
     </a>
     <i class="fa-solid fa-magnifying-glass" id="search-btn"></i>`;
   } else if (path.includes("profile.html") && user === profile?.name) {
     header.innerHTML = `
     <a href="feed.html">
-        <img src="assets/logo.svg" alt="Sippr logo" width="86" height="35"/>
+        <img src="assets/logo.svg" alt="Sippr logo" width="190" height="74"/>
     </a>
     <i class="fa-solid fa-arrow-right-from-bracket" id="logout"></i>`;
 
@@ -34,7 +34,7 @@ export function renderHeader() {
   } else {
     header.innerHTML = `
     <a href="feed.html">
-        <img src="assets/logo.svg" alt="Sippr logo" width="86" height="35"/>
+        <img src="assets/logo.svg" alt="Sippr logo" width="190" height="74"/>
     </a>`;
   }
 }
